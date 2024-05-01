88d4ace53f2e1c35fe73ad16144492a96d9478c0
bob4o-afk <bobkatasad@abv.bg>
2024-05-01 19:58:59 +0300

    Adding embedded part

---
+++ embedded/main.py
@@ -0,0 +1,39 @@
+import serial      #later for the arduino connection
+from time import sleep         #for delaying 
+import requests   #for the db
+from datetime import datetime
+# from gps import *
+from src.object_detection_image import recognize
+from src.camera import *
+import cv2
+
+
+def get_local_datetime():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def write_time_on_image(image_path):
+    image = cv2.imread(image_path)
+    if image is not None:
+        current_time = get_local_datetime()
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottom_left_corner = (10, image.shape[0] - 10)
+        font_scale = 3
+        font_color = (255, 255, 255)  # White color in BGR
+        line_type = 1
+        cv2.putText(image, current_time, bottom_left_corner, font, font_scale, font_color, line_type)
+        cv2.imwrite(image_path, image)
+    else:
+        print(f"Failed to load image from path: {image_path}")
+
+
+
+if "__main__" in __name__:
+    run_camera()
+    current_file = "images/image"
+    image_path = f"{current_file}_{get_current_image_id()}.jpg" 
+    write_time_on_image(image_path)
+    recognize()
+
+
+
---
+++ embedded/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install system-level dependencies
+sudo apt update
+sudo apt install -y python3-serial python3-requests python3-picamera2 python3-opencv
